1f5377de7a38a46ac9fac1e2db71527b779759d8
Matthew Wallace <wallacm4@tcd.ie>
2022-10-11 23:43:39 +0100

    User Input
    - Ask and read input
    - Quit system implemented

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        boolean quit = false;
+
+        while(!quit){
+            System.out.println("Enter equation to solve (or quit to close): ");
+            String request = input.next();
+            if(request.equalsIgnoreCase("quit"))
+                quit = true;
+            else {
+                
+            }
+        }
     }
 }
