c9c7bf44ee3a1c088924b7477be6157ad3f5ce92
francsir <72939196+francsir@users.noreply.github.com>
2022-10-13 13:32:09 +0100

    Edited main for easier unitTesting,Created UI.java

---
+++ src/CalcUI.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.junit.Test;
+
+
+@RunWith(JUnit4.class)
+public class CalcUI {
+    
+    @Test
+    public void testBasicOps()
+    {
+
+    }
+
+}
