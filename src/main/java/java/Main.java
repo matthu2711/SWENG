0bdac6ae68c8ecef51f1b5fe7b59029b485ba659
francsir <72939196+francsir@users.noreply.github.com>
2022-10-14 15:15:04 +0100

    CI working with JUNIT tests

--- src/main/java/java/Main.java
+++ src/main/java/java/Main.java
@@ -1,4 +1,4 @@
-package main.java;
+package java;
 
 import java.util.ArrayList;
 import java.util.Arrays;
--- src/test/java/java/testCalc.java
+++ src/test/java/java/testCalc.java
@@ -1,4 +1,4 @@
-package test.java;
+package java;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.runner.RunWith;
